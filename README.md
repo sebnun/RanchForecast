d56c8354b5dbc2d05b95e41c5369b48cf287c99d
Sebastian <sebnun@gmail.com>
2016-05-16 19:00:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RanchForecast
---
+++ RanchForecast/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RanchForecast
+//
+//  Created by Sebastian on 5/16/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
